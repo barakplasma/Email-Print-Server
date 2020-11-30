dc57877895abc8bcf5072d15c2cccf30cda2c0b2
Michael Salaverry <barakplasma@gmail.com>
2020-11-30 18:12:13 +0200

    fix: remove important detail

--- checkUCDprint.py
+++ checkUCDprint.py
@@ -4,7 +4,7 @@
 
 import imaplib
 mail = imaplib.IMAP4_SSL('imap.gmail.com')
-mail.login('ucdprint@gmail.com', 'itlm027test')
+mail.login('ucdprint@gmail.com', os.environ.get("password"))
 mail.list()
 # Out: list of "folders" aka labels in gmail.
 mail.select("inbox") # connect to inbox.
